4f9d558451dec14097cc213cbafb99930ed0e417
Shen1K <2997735722@qq.com>
2018-01-04 11:11:42 +0800

    Initial commit

---
+++ src/main/scala/cn/itcast/rpc/RemoteMessage.scala
@@ -0,0 +1,16 @@
+package cn.itcast.rpc
+
+trait RemoteMessage extends Serializable
+
+//Worker -> Master
+case class RegisterWorker(id: String, memory: Int, cores: Int) extends RemoteMessage
+case class Heartbeat(id: String ) extends RemoteMessage
+
+//Master -> Worker
+case class RegisteredWorker(masterUrl: String) extends RemoteMessage
+
+//Worker -> self
+case object SendHeartbeat
+
+//Master -> self
+case object  CheckTimeOutWorker
